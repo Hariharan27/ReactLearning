2db5ce701381ba395268bc5e5580f421daa39231
Hariharan <hariharan@ideas2it.com>
2020-06-01 15:44:57 +0530

    fix: drawer removal

--- src/container/root/App.js
+++ src/container/root/App.js
@@ -3,7 +3,6 @@ import { Provider } from "react-redux";
 import store from '../../redux/store/index'
 import Dashboard from '../dashboard/Dashboard'
 import Menu from '../../components/menu/Menu'
-import Drawer from '../../components/drawer/Drawer'
 
 function App() {
   return (
