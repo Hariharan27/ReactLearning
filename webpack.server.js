9dacf4dff8073b94986999dc1e29261fa102a6fa
Hariharan <hariharan@ideas2it.com>
2020-06-17 12:40:20 +0530

    feat: ssr rajasekar.

---
+++ webpack.server.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  entry: './server/index.js',
+
+  target: 'node',
+
+  externals: [nodeExternals()],
+
+  output: {
+    path: path.resolve('server-build'),
+    filename: 'index.js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader'
+      },{
+        test: /\.css$/,
+        use: [
+          'isomorphic-style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1
+            }
+          },
+          'postcss-loader'
+        ]
+      },{
+            test: /\.(jpe?g|png|gif|svg)$/i, 
+            loader: "file-loader?name=/src/assets/[name].[ext]"
+        }
+    ]
+  }
+};
